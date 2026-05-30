1db5f722c7bbdd8b63fc7a8eb094c91e3f3d8e77
hey-oise <oise.giwa.okugbe@gmail.com>
2026-05-30 19:11:59 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="flex flex-col text-center justify-center center w-full mt-60">
       <div className="m-auto flex flex-col gap-5">
-        <h1 className="text-3xl px-9 text-center text-blue-500 bold">run code lightwieght, even on zero ram
+        <h1 className="text-3xl px-9 text-center text-blue-500 bold">run code lightwieght, even on zero ram you have zero issues
         </h1>
         <div className="flex gap-2 m-auto">
         <Link href='/auth/sign-up' className='cursor-pointer border-gray-700 border p-2 rounded-md hover:bg-blue-700 bg-blue-600'>sign up</Link>
